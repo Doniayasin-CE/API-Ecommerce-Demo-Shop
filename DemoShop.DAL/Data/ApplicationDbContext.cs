ddc4b3bf7300b3d2304af493deac34c6ef341d6b
Donia Yasin <100124357+Doniayasin-CE@users.noreply.github.com>
2026-03-05 17:01:23 +0200

    N-Tier architecture and connection with the database

---
+++ DemoShop.DAL/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoShop.DAL.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+       public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+            : base(options)
+        {
+        }
+    }
+}
